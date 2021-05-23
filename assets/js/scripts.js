e237aa5e86b5c86b85c9bf7601103227f54f576a
HermineHoromyan <hermine.horomyan@gmail.com>
2021-05-23 17:28:06 +0400

    main block

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$('.main-block__video--form-play').click(function () {
+   var this_prt = $(this).parent()
+   var parent_video = $(this_prt)
+   var icon_video = $(parent_video).find(".main-block__video--form-play")
+   var video = $(parent_video).find(".video")
+   $(video).trigger('play')
+   $(video).attr("controls", "controls")
+   $(icon_video).fadeOut()
+});
