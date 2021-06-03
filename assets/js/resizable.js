568b938191839dd6d3cc2dc0f3fe56e6aba6a083
Жирайр Арутюнян <j.harutyunyan@tionit.com>
2021-06-03 16:05:07 +0400

    add table functional

--- assets/js/resizable.js
+++ assets/js/resizable.js
@@ -1,106 +1,74 @@
-let tables = document.getElementsByClassName('resizable');
+$(function () {
+   let pressed = false;
+   let start = undefined;
+   let startX, startWidth;
+
+   $('[data-table] th').mousedown(function (e) {
+      start = $(this);
+      pressed = true;
+      startX = e.pageX;
+      table = $(this).parent().parent().parent()
+      tableWidth = $(table).width()
+
+      startWidth = $(this).width();
+      $(start).addClass("resizing");
+      $(start).addClass("noSelect");
+   });
+
+   $(document).mousemove(function (e) {
+      if (pressed) {
+         $(start).width(startWidth + (e.pageX - startX));
+         $(table).width(tableWidth + (e.pageX - startX))
+      }
+   });
+
+   $(document).mouseup(function () {
+      if (pressed) {
+         $(start).removeClass("resizing");
+         $(start).removeClass("noSelect");
+         pressed = false;
+      }
+   });
+});
+
+let tables = document.querySelectorAll('[data-table]');
 for (let i = 0; i < tables.length; i++) {
    resizableGrid(tables[i]);
 }
 
 function resizableGrid(table) {
-   let row = table.getElementsByTagName('tr')[0],
-      cols = row ? row.children : undefined;
-   if (!cols) return;
-
-   table.style.overflow = 'hidden';
-
-   let tableHeight = table.offsetHeight;
-
-   for (let i = 0; i < cols.length; i++) {
-      let div = createDiv(tableHeight);
-      cols[i].appendChild(div);
-      cols[i].style.position = 'relative';
-      setListeners(div);
-   }
-
-   function setListeners(div) {
-      let pageX, curCol, nxtCol, curColWidth, nxtColWidth, tableWidth;
-
-      div.addEventListener('mousedown', function (e) {
-
-         tableWidth = document.getElementById('tableId').offsetWidth;
-         curCol = e.target.parentElement;
-         nxtCol = curCol.nextElementSibling;
-         pageX = e.pageX;
-
-         let padding = paddingDiff(curCol);
-
-         curColWidth = curCol.offsetWidth - padding;
-
-      });
-
-      div.addEventListener('mouseover', function (e) {
-         e.target.style.borderRight = '2px solid #0000ff';
-      })
-
-      div.addEventListener('mouseout', function (e) {
-         e.target.style.borderRight = '';
-      })
-
-      document.addEventListener('mousemove', function (e) {
-         if (curCol) {
-            let diffX = e.pageX - pageX;
-            curCol.style.width = (curColWidth + diffX) + 'px';
-            document.getElementById('tableId').style.width = tableWidth + diffX + "px"
-         }
-      });
-
-      document.addEventListener('mouseup', function (e) {
-         curCol = undefined;
-         nxtCol = undefined;
-         pageX = undefined;
-         nxtColWidth = undefined;
-         curColWidth = undefined
-      });
+   let row = table.getElementsByTagName('th')
+   for (let i = 0; i < row.length; i++) {
+      let div = createDiv();
+      row[i].appendChild(div);
+      row[i].style.position = 'relative';
    }
 
    function createDiv(height) {
       let div = document.createElement('div');
-      div.style.top = 0;
+      div.style.top = '50%';
+      div.style.transform = 'translate(0, -50%)'
       div.style.right = 0;
-      div.style.width = '5px';
+      div.style.width = '1px';
+      div.style.height = '25px'
       div.style.position = 'absolute';
-      div.style.cursor = 'ew-resize';
+      div.style.cursor = 'col-resize';
       div.style.userSelect = 'none';
       div.style.height = height + 'px';
-      div.style.zIndex = '999';
+      div.style.zIndex = '999'
+      div.style.backgroundColor = '#E1E1E1'
       return div;
-   }
-
-   function paddingDiff(col) {
-
-      if (getStyleVal(col, 'box-sizing') == 'border-box') {
-         return 0;
-      }
-
-      let padLeft = getStyleVal(col, 'padding-left');
-      let padRight = getStyleVal(col, 'padding-right');
-      return (parseInt(padLeft) + parseInt(padRight));
-
-   }
-
-   function getStyleVal(elm, css) {
-      return (window.getComputedStyle(elm, null).getPropertyValue(css))
    }
 };
 
+// $(document).ready(function () {
+//    // '.table-body' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
+//    $(window).on("load resize", function () {
+//       let scrollWidth = $('.table-block').width() - $('.table-block table').width();
+//       $('[data-table]').css({
+//          'padding-right': scrollWidth
+//       });
+//       console.log(scrollWidth)
+//    }).resize();
 
-
-
-$(document).ready(function () {
-   // '.table-body' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
-   $(window).on("load resize", function () {
-      var scrollWidth = $('.table-body').width() - $('.table-body table').width();
-      $('.table-header').css({
-         'padding-right': scrollWidth
-      });
-      console.log(scrollWidth)
-   }).resize();
-
-})
+// })
