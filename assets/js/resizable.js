8141600b1f6be5aef4009360442dc0f1e69caf9e
Жирайр Арутюнян <j.harutyunyan@tionit.com>
2021-06-09 22:51:15 +0400

    updatescroll js

--- assets/js/resizable.js
+++ assets/js/resizable.js
@@ -78,6 +78,32 @@ function resizableGrid(table) {
    }
 }
 
+
+// var speed = 0;
+// var scroll = 0;
+// var container = $('.carousel-frame');
+// var container_w = container.width();
+// var max_scroll = container[0].scrollWidth - container.outerWidth();
+
+// container.on('mousemove', function (e) {
+//    var mouse_x = e.pageX - container.offset().left;
+//    var mouseperc = 100 * mouse_x / container_w;
+//    speed = mouseperc - 50;
+// }).on('mouseleave', function () {
+//    speed = 0;
+// });
+
+// function updatescroll() {
+//    if (speed !== 0) {
+//       scroll += speed / 5;
+//       if (scroll < 0) scroll = 0;
+//       if (scroll > max_scroll) scroll = max_scroll;
+//       $('.carousel-frame').scrollLeft(scroll);
+//    }
+//    window.requestAnimationFrame(updatescroll);
+// }
+// window.requestAnimationFrame(updatescroll);
+
 // $(document).ready(function () {
 //    // '.table-body' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
 //    $(window).on("load resize", function () {
