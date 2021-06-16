611e2694b54bc08c0f40f66302d1f1a090bb3b80
Жирайр Арутюнян <j.harutyunyan@tionit.com>
2021-06-16 19:52:47 +0400

    codes refactoring - table sidebar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
    // Function for expand ui ===== -- Start
-   $('body').on('click', '[data-expand-click]', function () {
+   $('body').on('mousedown', '[data-expand-click]', function () {
       let this_data = $(this).attr('data-expand')
       let this_child = $(this).parent().find('[data-expand-box]:first')
       $('[data-outside-expand], [data-sub-outside-expand]').hide()
@@ -25,36 +25,62 @@ $(document).ready(function () {
       }
    });
 
-   $('body').on('click', '[data-sub-expand-click]', function () {
+   $('body').on('mousedown', '[data-sub-expand-click]', function () {
       let this_data = $(this).attr('data-expand')
       let this_child = $(this).parent().find('[data-expand-box]:first')
       $('[data-sub-outside-expand]').hide()
       $('[data-sub-not-outside-expand]').attr('data-expand', 'false')
-      // $('[data-sub-expand-outside-icon]').removeClass('rotate')
       let arrow = $(this).find('[data-sub-expand-icon]')
-      // let tableSidebarFilter = $(this).parent().find('[data-table-sidebar-filter]')
       if (this_data === 'true') {
          $(this).attr('data-expand', 'false')
          this_child.hide();
          $(arrow).removeClass('rotate')
-         // $(tableSidebarFilter).removeClass('active')
 
       } else if (this_data === 'false') {
          this_child.show();
          $(this).attr('data-expand', 'true')
          $(arrow).addClass('rotate')
-         // $(tableSidebarFilter).addClass('active')
       } else {
          this_child.hide();
       }
    });
-   $('body').on('click', '[data-expand-click-icon]', function () {
+   $('body').on('mousedown', '[data-expand-click-icon]', function () {
       $(this).toggleClass('rotate')
    })
    // Function for expand ui ===== -- End
 
+
+
+
+
+   // Function for toggle click ui ===== -- Start
+   $('body').on('mousedown', '[data-toggle-click]', function () {
+      let this_data = $(this).attr('data-toggle')
+      let this_child = $(this).parent().parent().parent().find('[data-toggle-box]')
+
+      let arrow = $(this, '[data-toggle-icon]')
+      if (this_data === 'true') {
+         $(this).attr('data-toggle', 'false')
+         this_child.hide();
+         $(arrow).removeClass('rotate')
+
+      } else if (this_data === 'false') {
+         this_child.show();
+         $(this).attr('data-toggle', 'true')
+         $(arrow).addClass('rotate')
+      } else {
+         this_child.hide();
+      }
+   })
+   // Function for toggle click ui ===== -- End
+
+
+
+
+
+
    // Function for change active ui ===== -- Start
-   $('body').on('click', '[data-change-active]', function () {
+   $('body').on('mousedown', '[data-change-active]', function () {
       let thic_parent = $(this).parent()
       let this_active = $(thic_parent).find('[data-change-active]')
       $(this_active).removeClass('active')
@@ -62,8 +88,12 @@ $(document).ready(function () {
    });
    // Function for change active ui ===== -- End
 
+
+
+
+
    // Function for modal ui ===== -- Start
-   $('body').on('click', '[data-open-modal]', function () {
+   $('body').on('mousedown', '[data-open-modal]', function () {
       let findModal = $('body').find('[data-modal-box]')
       let modalName = $(this).data('modalName')
       $(findModal).each(function () {
@@ -72,10 +102,14 @@ $(document).ready(function () {
          }
       })
    })
-   $('body').on('click', '[data-close-modal]', function () {
+   $('body').on('mousedown', '[data-close-modal]', function () {
       $('body').find('[data-modal-box]').hide()
    })
    // Function for modal ui ===== -- End
+
+
+
+
 
    // Function for table hover img tooltip ui ===== -- Start
    $('body').on('mouseenter', '[data-src-tooltip]', function () {
@@ -97,8 +131,13 @@ $(document).ready(function () {
    });
    // Function for table hover img tooltip ui ui ===== -- End
 
+
+
+
+
+
    // Function for window clicks anywhere outside blocks ui ===== -- Start
-   $(window).on('click', function (e) {
+   $(window).on('mousedown', function (e) {
       if (!$(e.target).closest('[data-not-outside]').length) {
          // clicks anywhere outside of the expand
          $('[data-outside-expand], [data-sub-outside-expand]').hide();
@@ -113,6 +152,9 @@ $(document).ready(function () {
       }
    });
    // Function for window clicks anywhere outside blocks ui ===== -- End
+
+
+
 
 
    // Function for daterangepicker ui ===== -- Start
